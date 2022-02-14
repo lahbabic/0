d3c8e1f9648c42127373c3a4bc8b362f3f4b629c
lahbabic <lahbabic@gmail.com>
2022-02-14 18:52:59 +0100

    Math powned.

--- math_bug.py
+++ math_bug.py
@@ -1,14 +1,13 @@
 import math
 import time
-
-number = []
-number1 = []
+exp = []
+log = []
 for i in range(1, 100):
-    number.append(math.exp(i)+math.log(i, 2))
-    number1.append(math.exp(i)-math.log(i, 2))
-a = (number[50] - number[0])/50
-a = (number1[50] - number1[0])/50
+    exp.append(math.exp(i)+math.log(i, 2))
+    log.append(math.exp(i)-math.log(i, 2))
+a = (exp[50] - exp[0])/50
+a = (log[50] - log[0])/50
 print(a)
-print(number[0])
+print(exp[0])
 print(a)
-print(number1[0])
+print(log[0])
