9d7d302bbfc66e1a3b1a592f10d2e958c9ce8972
lahbabic <lahbabic@gmail.com>
2022-02-14 18:45:28 +0100

    powned!

--- math_bug.py
+++ math_bug.py
@@ -5,20 +5,10 @@ number = []
 number1 = []
 for i in range(1, 100):
     number.append(math.exp(i)+math.log(i, 2))
-for i in range(1, 100):
     number1.append(math.exp(i)-math.log(i, 2))
-
-a = (number[98] - number[0])/98
+a = (number[50] - number[0])/50
+a = (number1[50] - number1[0])/50
 print(a)
 print(number[0])
-a = (number1[98] - number1[0])/98
 print(a)
 print(number1[0])
-
-
-
-
-
-
-
-
